0abd8dcd2d2612ab7690e3f7275232f065ea2779
大西雄貴 <yuki.onishi@88oct.co.jp>
2020-04-23 15:03:24 +0900

    stub スタブを組み込んだテスト

--- tests/unit/ParentWithAPICallChild.spec.js
+++ tests/unit/ParentWithAPICallChild.spec.js
@@ -4,7 +4,18 @@ import ComponentWithAsyncCall from '@/components/ComponentWithAsyncCall.vue'
 
 describe("ParentWithAPICallChild.vue", () => {
     it("renders with mount and does initialize API call", () => {
-        const wrapper = mount(ParentWithAPICallChild)
+        const wrapper = mount(ParentWithAPICallChild, {
+            stubs: {
+                ComponentWithAsyncCall: "<div class='stub'></div>"
+            }
+        })
+
+        expect(wrapper.find(ComponentWithAsyncCall).exists()).toBe(true)
+    })
+
+    it("renders with shallowMount and does initialize API call", () => {
+
+        const wrapper = shallowMount(ParentWithAPICallChild)
 
         expect(wrapper.find(ComponentWithAsyncCall).exists()).toBe(true)
     })
