8fbb8bd7600552f391df386e8833f319f3a266ad
大西雄貴 <yuki.onishi@88oct.co.jp>
2020-04-26 22:29:44 +0900

    Finding Components with name and Component

--- tests/unit/Parent.spec.js
+++ tests/unit/Parent.spec.js
@@ -1,5 +1,6 @@
 import { mount } from "@vue/test-utils"
 import Parent from "@/components/Parent.vue"
+import Child from "@/components/Child.vue"
 
 describe("Parent", () => {
     it("does not render a span", () => {
@@ -10,5 +11,21 @@ describe("Parent", () => {
         })
 
         expect(wrapper.find("span").isVisible()).toBe(false)
+    })
+
+    it("does not render a Child component", () => {
+        const wrapper = mount(Parent)
+        
+        expect(wrapper.find(Child).exists()).toBe(false)
+    })
+
+    it("renders a Child component", () => {
+        const wrapper = mount(Parent, {
+          data() {
+            return { showChild: true }
+          }
+        })
+      
+        expect(wrapper.find({ name: "Child" }).exists()).toBe(true)
     })
 })
