81fa16a6c24c6d77e3fca825361142439ff45bfd
大西雄貴 <yuki.onishi@88oct.co.jp>
2020-04-27 11:14:03 +0900

    find findAll

--- tests/unit/Parent.spec.js
+++ tests/unit/Parent.spec.js
@@ -1,6 +1,7 @@
 import { mount } from "@vue/test-utils"
 import Parent from "@/components/Parent.vue"
 import Child from "@/components/Child.vue"
+import ParentWithManyChildren from "@/components/ParentWithManyChildren.vue"
 
 describe("Parent", () => {
     it("does not render a span", () => {
@@ -27,5 +28,11 @@ describe("Parent", () => {
         })
       
         expect(wrapper.find({ name: "Child" }).exists()).toBe(true)
+    })
+
+    it("renders many children", () => {
+        const wrapper = mount(ParentWithManyChildren)
+      
+        expect(wrapper.findAll(Child).length).toBe(3)
     })
 })
