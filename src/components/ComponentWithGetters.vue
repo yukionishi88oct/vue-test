8696e62409ff6b50700901486d0286a9a42905be
大西雄貴 <yuki.onishi@88oct.co.jp>
2020-05-06 23:12:02 +0900

    Testing getters

---
+++ src/components/ComponentWithGetters.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fullname">
+    {{ fullname }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentWithGetters",
+
+  computed: {
+    fullname() {
+      return this.$store.getters.fullname;
+    },
+  },
+};
+</script>
--- tests/unit/ComponentWithVuex.spec.js
+++ tests/unit/ComponentWithVuex.spec.js
@@ -1,6 +1,7 @@
 import Vuex from "vuex";
 import { mount, createLocalVue } from "@vue/test-utils";
 import ComponentWithVuex from "@/components/ComponentWithVuex.vue";
+import ComponentWithGetters from "@/components/ComponentWithGetters.vue";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -9,6 +10,12 @@ localVue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     username: "alice",
+    firstName: "Alice",
+    lastName: "Doe",
+  },
+
+  getters: {
+    fullname: (state) => state.firstName + " " + state.lastName,
   },
 });
 
@@ -20,5 +27,36 @@ describe("ComponentWithVuex", () => {
     });
 
     expect(wrapper.find(".username").text()).toBe("alice");
+  });
+
+  it("renders a username using a real Vuex getter", () => {
+    const wrapper = mount(ComponentWithGetters, { store, localVue });
+
+    expect(wrapper.find(".fullname").text()).toBe("Alice Doe");
+  });
+
+  it("renders a username using computed mounting options", () => {
+    const wrapper = mount(ComponentWithGetters, {
+      // mockをmountの引数で定義
+      mocks: {
+        $store: {
+          getters: {
+            fullname: "Alice Doe",
+          },
+        },
+      },
+    });
+
+    expect(wrapper.find(".fullname").text()).toBe("Alice Doe");
+  });
+
+  it("renders a username using computed mounting options", () => {
+    const wrapper = mount(ComponentWithGetters, {
+      computed: {
+        fullname: () => "Alice Doe",
+      },
+    });
+
+    expect(wrapper.find(".fullname").text()).toBe("Alice Doe");
   });
 });
