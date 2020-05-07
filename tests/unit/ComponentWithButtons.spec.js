c50154fa4d0d54e775de9ccebf90fc1a98379131
大西雄貴 <yuki.onishi@88oct.co.jp>
2020-05-07 17:28:33 +0900

    Testing a Namespaced Action (or Mutation)

--- tests/unit/ComponentWithButtons.spec.js
+++ tests/unit/ComponentWithButtons.spec.js
@@ -26,4 +26,39 @@ describe("ComponentWithButtons", () => {
       { msg: "Test Commit" }
     );
   });
+
+  it("dispatches an action when a button is clicked", async () => {
+    const mockStore = { dispatch: jest.fn() };
+    const wrapper = mount(ComponentWithButtons, {
+      mocks: {
+        $store: mockStore,
+      },
+    });
+
+    wrapper.find(".dispatch").trigger("click");
+    await wrapper.vm.$nextTick();
+
+    expect(mockStore.dispatch).toHaveBeenCalledWith("testAction", {
+      msg: "Test Dispatch",
+    });
+  });
+
+  it("dispatch a namespaced action when button is clicked", async () => {
+    const store = new Vuex.Store();
+    store.dispatch = jest.fn();
+
+    const wrapper = mount(ComponentWithButtons, {
+      store,
+      localVue,
+    });
+
+    wrapper.find(".namespaced-dispatch").trigger("click");
+    await wrapper.vm.$nextTick();
+
+    expect(
+      store.dispatch
+    ).toHaveBeenCalledWith("namespaced/very/deeply/testAction", {
+      msg: "Test Namespaced Dispatch",
+    });
+  });
 });
